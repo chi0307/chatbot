cfb6325474e0f8848fd282b3159baa982afadce8
Junchi <benson26365156@gmail.com>
2021-06-11 17:57:38 +0800

    deploy

---
+++ precache-manifest.e95b9b7fcecc9bb9e6f629d642044be0.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d840aec9096964a0e2f8",
+    "url": "/chatbot/css/app.8d53b194.css"
+  },
+  {
+    "revision": "619e7b05987fef2f43e1",
+    "url": "/chatbot/css/chunk-vendors.24bf1742.css"
+  },
+  {
+    "revision": "b69de26b1083b536ede19de3bea0885a",
+    "url": "/chatbot/example.png"
+  },
+  {
+    "revision": "7682954c42df97c958f09f1b54e9047c",
+    "url": "/chatbot/index.html"
+  },
+  {
+    "revision": "d840aec9096964a0e2f8",
+    "url": "/chatbot/js/app.a3df3b46.js"
+  },
+  {
+    "revision": "619e7b05987fef2f43e1",
+    "url": "/chatbot/js/chunk-vendors.dc4b9d68.js"
+  },
+  {
+    "revision": "39596a9e6b45ef040113f99cb37b31bc",
+    "url": "/chatbot/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/chatbot/robots.txt"
+  },
+  {
+    "revision": "e5275ad626e9ed8418c4d5e93a630364",
+    "url": "/chatbot/service-worker-local.js"
+  },
+  {
+    "revision": "b2a720c31cbebd3fcd73109d3b019b89",
+    "url": "/chatbot/service-worker.js.bank"
+  }
+]);
